06b9c23ac035b64e5c472b238e674aee11ef6d7b
Israr4 <israrrajpoot34@gmail.com>
2023-02-08 15:26:21 +0500

    template changing

--- C++DevPrograms/template.cpp
+++ C++DevPrograms/template.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-using namespace std;
 #include <string>
 
 template <typename T>
-T min <T a,T b>
+T min (T a,T b)
 {
     return (a<b) ? a:b;
 }
 
 int main()
 {
-    std::cout<< min <int> (2,3)<<std::endl;
+    std::cout<< min (2,3)<<std::endl;
+    std::cout<< min (2.2,3.5)<<std::endl;
+    std::cout<< min ("Israr","Ali")<<std::endl;
     return 0;
 }
